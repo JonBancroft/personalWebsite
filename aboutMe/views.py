a4ef7f1e7d5f19e10cd090ede14097df43e5a19c
JonBancroft <JonathanBancroft29@gmail.com>
2024-02-25 02:10:22 -0700

    set up Project model

--- aboutMe/views.py
+++ aboutMe/views.py
@@ -1,29 +1,32 @@
 from django.shortcuts import render
+from . import models
 
 # Create your views here.
 def home(request):
-    return render(request, "index.html")
+    return render(request, "index.html", {'title': 'Jon Bancroft'})
 
 
 def all_projects(request):
-    return render(request, "projects.html")
+    projects = models.Project.objects.all()
+    return render(request, "projects.html", {'title': 'Projects',
+                                             'projects': projects})
 
 
 # TODO: update this
 def project(request, project_id):
-    return render(request, "projects.html")
+    return render(request, "projects.html", {'title': ''})
 
 
 def resume(request):
-    return render(request, "resume.html")
+    return render(request, "resume.html", {'title': 'Resume'})
 
 
 def about(request):
-    return render(request, "about.html")
+    return render(request, "about.html", {'title': 'About me :-)'})
 
 
 def contacts(request):
-    return render(request, "contacts.html")
+    return render(request, "contacts.html", {'title': 'Contacts'})
 
 
 def error_404(request, exception=None):
