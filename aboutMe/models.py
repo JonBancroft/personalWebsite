9018ea3ed0a7829c01e8a73b7efaa91094df9316
JonBancroft <JonathanBancroft29@gmail.com>
2024-03-03 16:20:18 -0700

    added spreadsheet images to projects

--- aboutMe/models.py
+++ aboutMe/models.py
@@ -7,9 +7,16 @@ class Project(models.Model):
     description = models.TextField(blank=True)
     thumbnail = models.ImageField(upload_to='images/')
     repo_link = models.URLField()
+    priority = models.IntegerField()
+
+    def __str__(self): 
+         return self.name
 
 
 class GalleryImage(models.Model):
     image = models.ImageField(upload_to="images/")
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.image.name
